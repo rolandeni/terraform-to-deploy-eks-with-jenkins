afd3b8ff08529664efe25613c8c955616a86b6f2
rolanduk <rolandul.re@gmail.com>
2023-03-16 15:13:59 +0100

    new files

---
+++ terraform/backend.tf
@@ -0,0 +1,13 @@
+terraform {
+  backend "s3" {
+    bucket = "s3roland007"
+    dynamodb_table = "state-lock"
+    key = "global/statefile/terraform.tfstate"
+    region = "us-east-2"
+    encrypt = true
+    
+  }
+
+}
+
+
