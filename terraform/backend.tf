b76565973c6ac44bfd975c8cf92b75e032dc4fe9
Roland Eni <59448052+rolandeni@users.noreply.github.com>
2023-03-16 23:28:41 +0100

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket = "s3roland007"
     dynamodb_table = "state-lock"
-    key = "global/statefile/terraform.tfstate"
+    key = "global/eks/statefile/terraform.tfstate"
     region = "us-east-2"
     encrypt = true
     
